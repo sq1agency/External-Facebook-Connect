c73b019b911382029fd0095d183b03666bf5c775
Rachel Guthrie <rachinabox@gmail.com>
2012-04-17 08:53:45 -0500

    another approach, still not working

--- index.php
+++ index.php
@@ -1,41 +1,38 @@
-<?php
-require_once "src/facebook.php";
-$facebook = new Facebook(array(
-        'appId'=>'326513144081660', // replace with your value
-        'secret'=>'2393a5b2aaec9de56dce79c68e004b0d' // replace with your value
-            ));
-
-			$user = $facebook->getUser();
-
-			if ($user) {
-			  try {
-			    $likes = $facebook->api("/me/likes/149214809611");
-			    if( !empty($likes['data']) )
-			        echo "I like!";
-			    else
-			        echo "not a fan!";
-			  } catch (FacebookApiException $e) {
-			    error_log($e);
-			    $user = null;
-			  }
-			}
-
-			if ($user) {
-			  $logoutUrl = $facebook->getLogoutUrl();
-			} else {
-			  $loginUrl = $facebook->getLoginUrl(array(
-			    'scope' => 'user_likes'
-			  ));
-			}
-
-			// rest of code here
-?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Practice Facebook App for the new EatWholly.com</title>
+		<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				$(button).click(function() {
+					FB.login(function(responseLogin) {
+					    if (responseLogin.session) {
+					        // user is logged in
+					        // uid is in responseLogin.session.uid
+					        // just explore the whole object with console.log(responseLogin);
+							alert('logged in');
+					    } else {
+					        // refused to grant the permissions'
+							alert('refused permissions');
+					    }
+					}, { perms:'offline_access,email' }); // example of perms
+				});
+			});
+		</script>
 	</head>
 	<body>
-
+		<div id="fb-root"></div>
+		    <script src="http://connect.facebook.net/en_US/all.js"></script>
+		    <script>
+		        FB.init({ 
+		            appId:'326513144081660', 
+		            cookie:true, 
+		            status:true, 
+		            xfbml:true,
+		            oauth:true
+		        });
+		    </script>
+		    <button>login to facebook</button>
 	</body>
 </html>
